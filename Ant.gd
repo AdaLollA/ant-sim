22469f8d56fb05b9e7abd81f5e3e2400dd3a6442
AdaLollA <lorenz.graaf@gmail.com>
2021-06-29 20:03:10 +0200

    ant wandering randomly

--- Ant.gd
+++ Ant.gd
@@ -1,10 +1,16 @@
 extends KinematicBody2D
 
 export var speed = 100
+export var wanderStrength = .1
 
 func _ready():
+	look_at(get_global_mouse_position())
 	pass
 
 func _process(delta):
-	look_at(get_global_mouse_position())
-	move_and_slide(Vector2(speed, 0).rotated(rotation))
+	# add randomness
+	rotate(rand_range(-wanderStrength, wanderStrength))
+	# forward
+	var velocity: Vector2 = Vector2(speed, 0).rotated(rotation)
+	# apply movement
+	move_and_slide(velocity)
