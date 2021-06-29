3c6b2df74d158dbc4720097719309d058e2238c4
AdaLollA <lorenz.graaf@gmail.com>
2021-06-29 20:31:48 +0200

    eating food

--- Ant.gd
+++ Ant.gd
@@ -1,16 +1,38 @@
 extends KinematicBody2D
 
-export var speed = 100
-export var wanderStrength = .1
+export var speed = 50
+export var wanderStrength = .05
+
+var target = null
 
 func _ready():
 	look_at(get_global_mouse_position())
 	pass
 
 func _process(delta):
+	# head for target if available
+	if target:
+		look_at(target)
+	
 	# add randomness
 	rotate(rand_range(-wanderStrength, wanderStrength))
+	
 	# forward
 	var velocity: Vector2 = Vector2(speed, 0).rotated(rotation)
 	# apply movement
 	move_and_slide(velocity)
+
+func _on_Left_area_entered(area):
+	pass # Replace with function body.
+func _on_Right_area_entered(area):
+	pass # Replace with function body.
+func _on_Front_area_entered(area):
+	pass # Replace with function body.
+
+func _on_Eyes_area_entered(area):
+	if 'Food' in area.get_parent().name:
+		target = area.get_global_position()
+
+func _on_Mouth_area_entered(area):
+	target = null
+	area.get_parent().queue_free()
